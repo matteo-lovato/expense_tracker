8d0b1ead5064b546df807c781dde9e6332938b54
matteo-lovato <lovato.teo@gmail.com>
2020-12-21 21:51:20 +0100

    added styles and details cards

--- src/App.js
+++ src/App.js
@@ -1,7 +1,32 @@
 import React from "react";
+import { Grid } from "@material-ui/core";
+import Details from "./components/Details/Details";
+import useStyles from "./styles";
 
 const App = () => {
-    return <div>App</div>;
+    const classes = useStyles();
+    return (
+        <div>
+            <Grid
+                className={classes.grid}
+                container
+                spacing={0}
+                alignItems="center"
+                justify="center"
+                style={{ height: "100vh" }}
+            >
+                <Grid item xs={12} sm={4}>
+                    <Details />
+                </Grid>
+                <Grid item xs={12} sm={4}>
+                    Main
+                </Grid>
+                <Grid item xs={12} sm={4}>
+                    <Details />
+                </Grid>
+            </Grid>
+        </div>
+    );
 };
 
 export default App;
