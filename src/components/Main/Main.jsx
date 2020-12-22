5f651fc2249f5fe6636e1b199e41d2b449745dc5
matteo-lovato <lovato.teo@gmail.com>
2020-12-22 21:52:36 +0100

    List component and style with dummy transactions

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -9,6 +9,7 @@ import {
 } from "@material-ui/core";
 import useStyles from "./styles";
 import Form from "./Form/Form";
+import List from "./List/List";
 
 const Main = () => {
     const classes = useStyles();
@@ -36,7 +37,7 @@ const Main = () => {
             <CardContent className={classes.cartContent}>
                 <Grid container spacing={2}>
                     <Grid item xs={12}>
-                        {/* List */}
+                        <List />
                     </Grid>
                 </Grid>
             </CardContent>
