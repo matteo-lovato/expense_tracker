e2d05df72c5b2a1860b4ea5ba6a247e0338465f6
matteo-lovato <lovato.teo@gmail.com>
2020-12-23 13:41:24 +0100

    store form data to the state

--- src/components/Main/Form/Form.jsx
+++ src/components/Main/Form/Form.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import {
     TextField,
     Typography,
@@ -12,8 +12,16 @@ import {
 
 import useStyles from "./styles";
 
+const initialState = {
+    amount: "",
+    category: "",
+    type: "Income",
+    date: new Date(),
+};
+
 const Form = () => {
     const classes = useStyles();
+    const [formData, setFormData] = useState(initialState);
     return (
         <Grid container spacing={2}>
             <Grid item xs={12}>
@@ -24,7 +32,12 @@ const Form = () => {
             <Grid item xs={6}>
                 <FormControl fullWidth>
                     <InputLabel>Type</InputLabel>
-                    <Select>
+                    <Select
+                        value={formData.type}
+                        onChange={e =>
+                            setFormData({ ...formData, type: e.target.value })
+                        }
+                    >
                         <MenuItem value="Income">Income</MenuItem>
                         <MenuItem value="Expense">Expense</MenuItem>
                     </Select>
@@ -33,17 +46,39 @@ const Form = () => {
             <Grid item xs={6}>
                 <FormControl fullWidth>
                     <InputLabel>Category</InputLabel>
-                    <Select>
+                    <Select
+                        value={formData.category}
+                        onChange={e => {
+                            setFormData({
+                                ...formData,
+                                category: e.target.value,
+                            });
+                        }}
+                    >
                         <MenuItem value="Business">Business</MenuItem>
                         <MenuItem value="Salary">Salary</MenuItem>
                     </Select>
                 </FormControl>
             </Grid>
             <Grid item xs={6}>
-                <TextField type="number" label="Amount" />
+                <TextField
+                    type="number"
+                    label="Amount"
+                    value={formData.amount}
+                    onChange={e =>
+                        setFormData({ ...formData, amount: e.target.value })
+                    }
+                />
             </Grid>
             <Grid item xs={6}>
-                <TextField type="date" label="Date" />
+                <TextField
+                    type="date"
+                    label="Date"
+                    value={formData.Date}
+                    onChange={e =>
+                        setFormData({ ...formData, date: e.target.value })
+                    }
+                />
             </Grid>
             <Button
                 className={classes.button}
