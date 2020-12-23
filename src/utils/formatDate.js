1f30a2ac8740aea9b42b85b75b4d794fbc216b31
matteo-lovato <lovato.teo@gmail.com>
2020-12-23 21:19:16 +0100

    add function that formats the date

---
+++ src/utils/formatDate.js
@@ -0,0 +1,19 @@
+const formatDate = date => {
+    const d = new Date(date);
+    // months start at 0
+    let month = `${d.getMonth() + 1}`;
+    let day = `${d.getDate()}`;
+    const year = `${d.getFullYear()}`;
+
+    // 01 instead of 1 for the month
+    if (month.length < 2) {
+        month = `0${month}`;
+    }
+    // 04 instead of 4 for the day
+    if (day.length < 2) {
+        day = `0${day}`;
+    }
+    return [day, month, year].join("-");
+};
+
+export default formatDate;
