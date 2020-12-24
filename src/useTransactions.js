40d3313195e8c2c18e82c68cd7043e414cad041d
matteo-lovato <lovato.teo@gmail.com>
2020-12-24 22:00:19 +0100

    create custom hook useTransactions to manage transactions' data

---
+++ src/useTransactions.js
@@ -0,0 +1,48 @@
+// logic to calcultate transactions data
+import { useContext } from "react";
+import { ExpenseTrackerContext } from "./context/context";
+import {
+    incomeCategories,
+    expenseCategories,
+    resetCategories,
+} from "./constants/categories";
+
+const useTransactions = title => {
+    // reset to 0
+    resetCategories();
+    // destructure transactions from the context
+    const { transactions } = useContext(ExpenseTrackerContext);
+    // get the same category's type of the title ( income or expense )
+    const transactionsPerType = transactions.filter(t => t.type === title);
+    // loop through all the categories and sum the amounts
+    const total = transactionsPerType.reduce(
+        (accumulator, currVal) => (acc += currVal.amount),
+        0
+    );
+    // all the categories of Income or Expense type based on title
+    const categories =
+        title === "Income" ? incomeCategories : expenseCategories;
+
+    // updated amonts for each category
+    transactionsPerType.forEach(t => {
+        const category = categories.find(c => (c.type = t.type));
+        category?.amount += t.amount;
+    });
+
+    // remove categories with amount = 0
+    const filteredCategories = categories.filter(c => c.amount > 0);
+
+    const chartData = {
+        datasets: [
+            {
+                data: filteredCategories.map(c => c.amount),
+                backgroundColor: filteredCategories.map(c => c.color),
+            },
+        ],
+        labels: filteredCategories.map(c => c.type),
+    };
+
+    return { filteredCategories, total, chartData };
+};
+
+export default useTransactions;
