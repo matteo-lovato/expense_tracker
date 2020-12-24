95ee92c5a1254408be519673b22776d2d2b143fe
matteo-lovato <lovato.teo@gmail.com>
2020-12-24 23:29:08 +0100

    imported speechly

--- src/index.js
+++ src/index.js
@@ -3,10 +3,16 @@ import ReactDOM from "react-dom";
 import App from "./App";
 import "./index.css";
 import { Provider } from "./context/context";
+import { SpeechProvider } from "@speechly/react-client";
 
 ReactDOM.render(
-    <Provider>
-        <App />
-    </Provider>,
+    <SpeechProvider
+        appId={process.env.REACT_APP_SPEECHLY_APP_ID}
+        language="en-US"
+    >
+        <Provider>
+            <App />
+        </Provider>
+    </SpeechProvider>,
     document.getElementById("root")
 );
