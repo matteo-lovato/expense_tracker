e825dc1a8e54f04dfc920948b2eca8111048345d
matteo-lovato <lovato.teo@gmail.com>
2020-12-25 10:56:27 +0100

    add transactions with voice

--- src/components/Main/Form/Form.jsx
+++ src/components/Main/Form/Form.jsx
@@ -69,7 +69,27 @@ const Form = () => {
                         setFormData({ ...formData, amount: e.value });
                         break;
                     case "category":
-                        setFormData({ ...formData, category });
+                        if (
+                            incomeCategories
+                                .map(iC => iC.type)
+                                .includes(category)
+                        ) {
+                            setFormData({
+                                ...formData,
+                                type: "Income",
+                                category,
+                            });
+                        } else if (
+                            expenseCategories
+                                .map(iC => iC.type)
+                                .includes(category)
+                        ) {
+                            setFormData({
+                                ...formData,
+                                type: "Expense",
+                                category,
+                            });
+                        }
                         break;
                     case "date":
                         setFormData({ ...formData, date: e.value });
@@ -78,6 +98,15 @@ const Form = () => {
                         break;
                 }
             });
+            if (
+                segment.isFinal &&
+                formData.amount &&
+                formData.category &&
+                formData.type &&
+                formData.date
+            ) {
+                createTransaction();
+            }
         }
     }, [segment]);
 
