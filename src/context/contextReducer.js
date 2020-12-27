509497708b92e9f5b00d10a26f2f1ee48e080e0a
matteo-lovato <lovato.teo@gmail.com>
2020-12-27 15:17:56 +0100

    local storage

--- src/context/contextReducer.js
+++ src/context/contextReducer.js
@@ -1,16 +1,22 @@
 // Reducer => a function that takes in the old state and an action => new state
 
 const contextReducer = (state, action) => {
+    let transactions;
     switch (action.type) {
         case "DELETE_TRANSACTION":
             // to delete you filter and copy all the items except the one with the id you want to remove
-            const transactions = state.filter(
+            transactions = state.filter(
                 transaction => transaction.id !== action.payload
             );
+
+            localStorage.setItem("transactions", JSON.stringify(transactions));
+
             return transactions;
         case "ADD_TRANSACTION":
             // adding the new element at the start since new transaction will pop up at the beginning of the list
-            return [action.payload, ...state];
+            transactions = [action.payload, ...state];
+            localStorage.setItem("transactions", JSON.stringify(transactions));
+            return transactions;
         default:
             return state;
     }
