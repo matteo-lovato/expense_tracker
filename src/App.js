08367c037748964eea67f9e5aececedb818e5450
matteo-lovato <lovato.teo@gmail.com>
2020-12-27 15:56:16 +0100

    scroll to top if using voice commands on mobile

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
-import React from "react";
+import React, { useRef, useEffect } from "react";
 import { Grid } from "@material-ui/core";
+import { SpeechState, useSpeechContext } from "@speechly/react-client";
 import Details from "./components/Details/Details";
 import Main from "./components/Main/Main";
 import useStyles from "./styles";
@@ -11,6 +12,15 @@ import {
 
 const App = () => {
     const classes = useStyles();
+    const { speechState } = useSpeechContext();
+    const main = useRef(null);
+    const executeScroll = () => main.current.scrollIntoView();
+    useEffect(() => {
+        if (speechState === SpeechState.Recording) {
+            executeScroll();
+        }
+    }, [speechState]);
+
     return (
         <div>
             <Grid
@@ -24,7 +34,7 @@ const App = () => {
                 <Grid item xs={12} sm={4} className={classes.mobile}>
                     <Details title="Income" />
                 </Grid>
-                <Grid item xs={12} sm={3} className={classes.main}>
+                <Grid ref={main} item xs={12} sm={3} className={classes.main}>
                     <Main />
                 </Grid>
                 <Grid item xs={12} sm={4} className={classes.desktop}>
